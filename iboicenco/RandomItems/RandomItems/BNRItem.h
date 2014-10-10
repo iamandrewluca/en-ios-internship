323c8fdb0d8cca9f7f91fce16552c655d916a082
iur4ik <iur4ik7@gmail.com>
2014-10-10 13:44:12 +0300

    Initial Commit

---
+++ iboicenco/RandomItems/RandomItems/BNRItem.h
@@ -0,0 +1,13 @@
+//
+//  BNRItem.h
+//  RandomItems
+//
+//  Created by iboicenco on 10/9/14.
+//  Copyright (c) 2014 iboicenco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItem : NSObject
+
+@end
---
+++ iboicenco/RandomItems/RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by iboicenco on 10/9/14.
+//  Copyright (c) 2014 iboicenco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
