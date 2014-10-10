7d86f915d9da0476841c97733b905fd46c89da1f
iur4ik <iur4ik7@gmail.com>
2014-10-10 13:45:36 +0300

    Implementing randomItems app with bronze and silver challenges

--- iboicenco/RandomItems/RandomItems/BNRItem.h
+++ iboicenco/RandomItems/RandomItems/BNRItem.h
@@ -8,6 +8,38 @@
 
 #import <Foundation/Foundation.h>
 
+#pragma mark - instance variables
 @interface BNRItem : NSObject
+{
+    NSString *_itemName;
+    NSString *_serialNumber;
+    int _valueInDollars;
+    NSDate *_dateCreated; // readonly
+}
+
+#pragma mark - class method
++(instancetype)randomItem;
+
+#pragma mark - initializers
+// Designated initializer for BNRItem
+-(instancetype)initWithItemName:(NSString *)name
+                 valueInDollars:(int)value
+                   serialNumber:(NSString *)sNumber;
+
+
+-(instancetype)initWithItemName:(NSString *)name;
+
+
+#pragma mark - accessors methods
+- (void)setItemName:(NSString *)str;
+- (NSString *)itemName;
+
+- (void)setSerialNumber:(NSString *)str;
+- (NSString *)serialNumber;
+
+- (void)setValueInDollars:(int)v;
+- (int)valueInDollars;
+
+- (NSDate *)dateCreated;
 
 @end
