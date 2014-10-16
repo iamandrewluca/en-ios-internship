8110876f38577ec547c515352886dd34d5caedd6
Andrei Luca <thendrluca@gmail.com>
2014-10-16 16:57:28 +0300

    Homepwner Initial commit

---
+++ aluca/BNRHomepwner/BNRHomepwner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BNRHomepwner
+//
+//  Created by Andrei Luca on 10/16/14.
+//  Copyright (c) 2014 Andrei Luca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
