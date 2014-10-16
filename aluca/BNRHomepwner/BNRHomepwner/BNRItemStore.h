ba6beb565b11ecb1a533c67e6c34ab8c5c83e050
Andrei Luca <thendrluca@gmail.com>
2014-10-16 18:17:25 +0300

    Added ItemStore

---
+++ aluca/BNRHomepwner/BNRHomepwner/BNRItemStore.h
@@ -0,0 +1,20 @@
+//
+//  BNRItemStore.h
+//  BNRHomepwner
+//
+//  Created by Andrei Luca on 10/16/14.
+//  Copyright (c) 2014 Andrei Luca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class BNRItem;
+
+@interface BNRItemStore : NSObject
+
+@property (nonatomic, readonly) NSArray *allItems;
+
++ (instancetype)sharedStore;
+- (BNRItem *)createItem;
+
+@end
---
+++ aluca/BNRHomepwner/BNRHomepwner/BNRItemStore.m
@@ -0,0 +1,63 @@
+//
+//  BNRItemStore.m
+//  BNRHomepwner
+//
+//  Created by Andrei Luca on 10/16/14.
+//  Copyright (c) 2014 Andrei Luca. All rights reserved.
+//
+
+#import "BNRItemStore.h"
+#import "BNRItem.h"
+
+@interface BNRItemStore ()
+
+@property (nonatomic) NSMutableArray *privateItems;
+
+@end
+
+@implementation BNRItemStore
+
++ (instancetype)sharedStore
+{
+    static BNRItemStore *sharedStore = nil;
+    
+    if (!sharedStore) {
+        sharedStore = [[self alloc] initPrivate];
+    }
+    
+    return  sharedStore;
+}
+
+- (instancetype)init
+{
+    @throw [NSException exceptionWithName:@"Singelton" reason:@"Use +[BNRItemStore sharedStore]" userInfo:nil];
+
+    return nil;
+}
+
+- (instancetype)initPrivate
+{
+    self = [super init];
+    
+    if (self) {
+        _privateItems = [[NSMutableArray alloc] init];
+    }
+    
+    return self;
+}
+
+- (NSArray *)allItems
+{
+    return self.privateItems;
+}
+
+- (BNRItem *)createItem
+{
+    BNRItem *item = [BNRItem randomItem];
+    
+    [self.privateItems addObject:item];
+    
+    return item;
+}
+
+@end
