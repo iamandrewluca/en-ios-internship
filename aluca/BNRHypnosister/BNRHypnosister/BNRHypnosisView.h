c010772521c7c7cf38487b0a91a27a6766f05286
Andrei Luca <thendrluca@gmail.com>
2014-10-23 17:53:43 +0300

    initial commit: Hypnosister app as new branch

---
+++ aluca/BNRHypnosister/BNRHypnosister/BNRHypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisView.h
+//  BNRHypnosister
+//
+//  Created by Andrei Luca on 10/9/14.
+//  Copyright (c) 2014 Andrei Luca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRHypnosisView : UIView
+
+@end
