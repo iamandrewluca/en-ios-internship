99ef04710fa8fd4a5d7a3d694d3c3d6d7d7d92f2
Andrei Luca <thendrluca@gmail.com>
2014-10-28 23:59:08 -0700

    Implemented fetchFeed method

--- aluca/BNRNerdFeed/BNRNerdFeed/BNRCoursesViewController.m
+++ aluca/BNRNerdFeed/BNRNerdFeed/BNRCoursesViewController.m
@@ -10,9 +10,51 @@
 
 @interface BNRCoursesViewController () <UITableViewDelegate>
 
+@property (nonatomic) NSURLSession *session;
+
 @end
 
 @implementation BNRCoursesViewController
+
+#pragma mark Other
+
+- (void)fetchFeed
+{
+    NSString *requestString = @"http://bookapi.bignerdranch.com/courses.json";
+    NSURL *url = [NSURL URLWithString:requestString];
+    NSURLRequest *req = [NSURLRequest requestWithURL:url];
+    
+    NSURLSessionDataTask *dataTask = [self.session dataTaskWithRequest:req completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        
+        NSString *json = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"%@", json);
+        
+    }];
+    
+    [dataTask resume];
+}
+
+#pragma  mark Init
+
+- (instancetype)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    
+    if (self) {
+        
+        self.navigationItem.title = @"BNRCourses";
+        
+        NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+        _session = [NSURLSession sessionWithConfiguration:config delegate:nil delegateQueue:nil];
+        
+        [self fetchFeed];
+        
+    }
+    
+    return self;
+}
+
+#pragma mark TableView
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
