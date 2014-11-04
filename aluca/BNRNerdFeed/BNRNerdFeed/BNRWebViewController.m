16b71e33f0613ded5633cfce4d101d2c00ca63f7
Andrei Luca <thendrluca@gmail.com>
2014-11-04 18:24:27 +0200

    chapter 21 challenges partially implemented

--- aluca/BNRNerdFeed/BNRNerdFeed/BNRWebViewController.m
+++ aluca/BNRNerdFeed/BNRNerdFeed/BNRWebViewController.m
@@ -16,9 +16,23 @@
 {
     UIWebView *webView = [[UIWebView alloc] init];
     
+    CGRect toolbaFrame = CGRectMake(0, 50, webView.frame.size.width, 50);
+    
+    UIToolbar *toolbar = [[UIToolbar alloc] init];
+    
+    [webView addSubview:toolbar];
+    
+    UIBarButtonItem *backwardButton = [[UIBarButtonItem alloc] initWithTitle:@"Backward" style:UIBarButtonItemStylePlain target:self action:@selector(backwardWebView)];
+    UIBarButtonItem *forwardButton = [[UIBarButtonItem alloc] initWithTitle:@"Forward" style:UIBarButtonItemStylePlain target:self action:@selector(forwardWebView)];
+    
+    NSArray *toolbarButtons = [NSArray arrayWithObjects:backwardButton, forwardButton, nil];
+    
+    [toolbar setItems:toolbarButtons];
+    
     webView.scalesPageToFit = YES;
     
     self.view = webView;
+
 }
 
 #pragma mark Set
@@ -32,6 +46,23 @@
         
         [(UIWebView *)self.view loadRequest:req];
     }
+}
+
+#pragma mark Buttons
+
+- (void)backwardWebView
+{
+    if (((UIWebView *)self).canGoBack) {
+        [((UIWebView *)self) goBack];
+    }
+}
+
+- (void)forwardWebView
+{
+    if (((UIWebView *)self).canGoForward) {
+        [((UIWebView *)self) goForward];
+    }
+
 }
 
 @end
