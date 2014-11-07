c0444b0ae019726b18389c345a09283620c159e8
Andrei Luca <thendrluca@gmail.com>
2014-11-07 16:41:45 +0200

    restored Quiz app

---
+++ aluca/Quiz/Quiz/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by Andrei Luca on 10/7/14.
+//  Copyright (c) 2014 Andrei Luca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
