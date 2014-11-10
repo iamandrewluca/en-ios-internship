04e4f434797d37a8c353d10f15fd7fa878f603ef
Andrei Luca <thendrluca@gmail.com>
2014-11-10 14:47:18 +0200

    initial commit

---
+++ common/ENote/ENote/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ENote
+//
+//  Created by Andrei Luca on 11/10/14.
+//  Copyright (c) 2014 Endava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
