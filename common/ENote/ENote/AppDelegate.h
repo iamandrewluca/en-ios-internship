c237b39dd30e42dc2f2c10347f4f57d5ab402fda
Andrei Luca <thendrluca@gmail.com>
2014-12-15 10:48:59 +0200

    handling device shake

--- common/ENote/ENote/AppDelegate.h
+++ common/ENote/ENote/AppDelegate.h
@@ -9,7 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @class NotesCollectionViewController;
-@class SWRevealViewController;
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
---
+++ common/ENote/ENote/UIViewController+HandleShake.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+HandleShake.h
+//  ENote
+//
+//  Created by Andrei Luca on 12/15/14.
+//  Copyright (c) 2014 Endava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (HandleShake)
+
+@end
